910ca73fb7063e0e618e3682c387056f81f5d8b0
unexCoder <unexCoder@Micaelas-MacBook-Pro.local>
2025-03-19 21:58:13 -0300

    first controller and view implementation

--- src/main/java/com/unexcoder/solar_energia/controladores/ArticuloControlador.java
+++ src/main/java/com/unexcoder/solar_energia/controladores/ArticuloControlador.java
@@ -1,5 +1,27 @@
 package com.unexcoder.solar_energia.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.unexcoder.solar_energia.servicios.ArticuloServicio;
+
+@Controller
+@RequestMapping("/articulo")
 public class ArticuloControlador {
     
+    @Autowired
+    ArticuloServicio articuloServicio;
+
+    @GetMapping("/registrar")
+    public String registrar() {
+        return "articulo.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro() {
+        return "articulo.html";
+    }
 }
--- src/main/java/com/unexcoder/solar_energia/controladores/PortalControlador.java
+++ src/main/java/com/unexcoder/solar_energia/controladores/PortalControlador.java
@@ -1,5 +1,32 @@
 package com.unexcoder.solar_energia.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.unexcoder.solar_energia.servicios.UsuarioServicio;
+
+@Controller
+@RequestMapping("/")
 public class PortalControlador {
+
+    @Autowired
+    private UsuarioServicio usuarioServicio;
     
+    @GetMapping("/index")
+    public String index() {
+        return "index.html";
+        // return "redirect:/login";
+    }
+    @GetMapping("/registrar")
+    public String registrar() {
+        return "registro.html";
+    }
+
+    @PostMapping("/registro")
+    public String registro() {
+        return "registro.html";
+    }
 }
